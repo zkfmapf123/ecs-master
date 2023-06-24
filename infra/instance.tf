dfcb1cbfb764caa804b4417c54535c6caf325278
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-06-24 13:14:28 +0900

    [ADD] vpc

---
+++ infra/instance.tf
@@ -0,0 +1,26 @@
+#### Backend ####
+terraform {
+  backend "s3" {
+    bucket = "my-ecs-terraform"
+    key    = "2023-06-24/terraform.tfstate"
+    region = "ap-northeast-2"
+  }
+}
+
+#### Variable ####
+variable "region" {
+  default = "ap-northeast-2"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+#### Instance #####
+module "vpc" {
+  source = "./modules/vpc"
+
+  region      = var.region
+  env         = var.env
+  config_json = jsondecode(file("./config/vpc.json"))
+}
---
+++ infra/modules/vpc/main.tf
@@ -0,0 +1,75 @@
+variable "region" {}
+variable "env" {}
+variable "config_json" {}
+
+locals {
+  config = var.config_json
+
+  azs = [for az in local.config.azs : "${var.region}${az}"]
+
+  publics = {
+    for i, az in local.azs :
+    az => local.config.public_cidrs[i]
+  }
+
+  privates = {
+    for i, az in local.azs :
+    az => local.config.private_cidrs[i]
+  }
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = local.config.vpc_cidr_block
+
+  tags = {
+    Name = "${var.env}-${local.config.vpc_name}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "publics" {
+  for_each = local.publics
+
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = each.key
+  cidr_block        = each.value
+
+  tags = {
+    Name = "${var.env}-public-${each.key}"
+  }
+}
+
+resource "aws_subnet" "privates" {
+  for_each = local.privates
+
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = each.key
+  cidr_block        = each.value
+
+  tags = {
+    Name = "${var.env}-private-${each.key}"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.env}-public-igw"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  for_each = aws_subnet.publics
+
+  subnet_id     = each.value.id
+  route_table_id = aws_route_table.public_route_table.id
+}
